2cb649131350ce2e719d11ae55271a633c25d3b9
Vladimir RM <shvdrummer@gmail.com>
2023-05-09 16:04:48 +0300

    Create block Post

--- backend/models/post.js
+++ backend/models/post.js
@@ -1,1 +1,11 @@
 import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema({
+    title:{type:String,require:true,},
+    text:{type:String,require:true,},
+})
+
+
+const Post = mongoose.model("Post",postSchema)
+
+export default Post;
