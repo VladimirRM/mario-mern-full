ba70295403b341c704783dc89174956347bb4e43
Vladimir RM <shvdrummer@gmail.com>
2023-05-09 16:00:39 +0300

    create dir models

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 import  express  from "express";
-import mongoose from "mongoose";
 import  cors from "cors";
 import   "colors";
 import   morgan from 'morgan'
 console.log( "1");
 
 const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(morgan('dev'))
