572842c2f14819805402b77445634e293f7b5fed
Vladimir RM <shvdrummer@gmail.com>
2023-05-09 15:57:10 +0300

    Import main

---
+++ backend/server.js
@@ -0,0 +1,8 @@
+import  express  from "express";
+import mongoose from "mongoose";
+import  cors from "cors";
+import   "colors";
+import   morgan from 'morgan'
+console.log( "1");
+
+const app = express()
